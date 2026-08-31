a9cca16fe30ed496889259d996923175f8062839
tuangiangluong-afk <292916237+tuangiangluong-afk@users.noreply.github.com>
2026-08-31 13:04:00 +0200

    chore(seo): remove LinkedIn from sameAs

--- src/components/SchemaJSON.tsx
+++ src/components/SchemaJSON.tsx
@@ -73,9 +73,6 @@ export default function SchemaJSON({ type, site, breadcrumbItems, faqSegment }: 
             "name": "Expert Thermostat Copropriété",
             "url": baseUrl,
             "logo": `${baseUrl}/logo.png`,
-            "sameAs": [
-                "https://www.linkedin.com/company/thermostat-copropriete"
-            ],
             "contactPoint": {
                 "@type": "ContactPoint",
                 "telephone": site.phoneNumber || "01 84 80 00 00",
